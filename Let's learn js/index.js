99608bfa3a1e4f80d2e4e6f9dd6c67d61bf5e56a
Kisal Nelaka <kisalnelaka6@gmail.com>
2025-02-20 12:52:26 +0530

    basics done.
    did the basic stuff like console.log and window alert and document.getElementById .
    now studying variables

---
+++ Let's learn js/index.js
@@ -0,0 +1,26 @@
+// console.log('Hello, World!');
+// console.log(`another console log`);
+// // alert('Hello, World!');
+// //alert(`Hello, World! 2`);
+// //window.alert('Hello, World!');
+// document.getElementById("h1id").textContent = 'Hello, World!';
+// document.getElementById("pid").textContent = 'lorem ipsum';
+
+// --------------------------------------------------
+
+let x;
+x=10;
+console.log(x);
+//window.alert(x);
+let y=20;
+console.log(`y is ${y}`); //only backticks work it seems
+console.log("y is " + y); //this works too with double quotes
+console.log('y is ' + y); //this works too with single quotes
+
+let age=29;
+let price=19.99;
+let name="kisal";
+let isOnline=true;
+console.log(typeof name);
+console.log(`my name is ${name} and I am ${age} years old`);
+console.log(typeof isOnline);
