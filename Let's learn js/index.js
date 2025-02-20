2c6466cc7938f3d4cc6e03fe114bfbe8fcc1bc62
Kisal Nelaka <kisalnelaka6@gmail.com>
2025-02-20 21:36:00 +0530

    type conversion done

--- Let's learn js/index.js
+++ Let's learn js/index.js
@@ -77,3 +77,19 @@
 
 //---------------------------------------------------------------
 
+// let age = window.prompt("Enter your age");
+// age = Number(age);
+// age+=1;
+// console.log(age, typeof age);
+// let x = "";
+// let y = "";
+// let z = "";	
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+//---------------------------------------------------------------
