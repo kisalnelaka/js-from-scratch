3e3010b87cd8f8c20e6be2a2db62a38e964a7042
Kisal Nelaka <kisalnelaka6@gmail.com>
2025-02-20 22:41:30 +0530

    Simple counter with js made
    Built a simple number counter with js

--- Let's learn js/index.js
+++ Let's learn js/index.js
@@ -119,3 +119,25 @@
 // };
 
 //-------------------------------------------------------------------
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLbl = document.getElementById("countLbl");
+
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count++;
+    countLbl.textContent = count;
+}
+
+decreaseBtn.onclick = function(){
+    count--;
+    countLbl.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count=0;
+    countLbl.textContent = count;
+}
